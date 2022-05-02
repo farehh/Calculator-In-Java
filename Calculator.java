385047704caa235c4857acf6ba50d411c3c6fd9d
Fareh Iqbal <fareh.legend@gmail.com>
2022-05-02 23:59:12 +0500

    not working on this

--- Calculator.java
+++ Calculator.java
@@ -10,6 +10,8 @@ public class Calculator implements ActionListener{
     JButton[] numButtons = new JButton[10];
 
     JButton addButton, subButton, mulButton, divButton, decButton, equButton;
+    JButton aButton, bButton, cButton, dButton, eButton, fButton;
+    JButton clrButton;
 
     Font font= new Font("Courier", Font.BOLD, 20);
 
@@ -38,14 +40,20 @@ public class Calculator implements ActionListener{
         textfield.setFont(font);
 
 
-
-
         addButton = new JButton("+");
 		subButton = new JButton("-");
 		mulButton = new JButton("*");
 		divButton = new JButton("/");
 		decButton = new JButton(".");
 		equButton = new JButton("=");
+
+        aButton = new JButton("A");
+        bButton = new JButton("B");
+        cButton = new JButton("C");
+        dButton = new JButton("D");
+        eButton = new JButton("E");
+        fButton = new JButton("F");
+        clrButton = new JButton("⌫");
 
         addButton.setBackground(Color.DARK_GRAY);
         subButton.setBackground(Color.DARK_GRAY);
@@ -54,12 +62,31 @@ public class Calculator implements ActionListener{
         decButton.setBackground(Color.DARK_GRAY);
         equButton.setBackground(Color.DARK_GRAY);
 
+        aButton.setBackground(Color.DARK_GRAY);
+        bButton.setBackground(Color.DARK_GRAY);
+        cButton.setBackground(Color.DARK_GRAY);
+        dButton.setBackground(Color.DARK_GRAY);
+        eButton.setBackground(Color.DARK_GRAY);
+        fButton.setBackground(Color.DARK_GRAY);
+        clrButton.setBackground(Color.DARK_GRAY);
+        equButton.setBackground(Color.DARK_GRAY);
+
         addButton.setForeground(Color.WHITE);
         subButton.setForeground(Color.WHITE);
         mulButton.setForeground(Color.WHITE);
         divButton.setForeground(Color.WHITE);
         decButton.setForeground(Color.WHITE);
         equButton.setForeground(Color.WHITE);
+
+        aButton.setForeground(Color.WHITE);
+        bButton.setForeground(Color.WHITE);
+        cButton.setForeground(Color.WHITE);
+        dButton.setForeground(Color.WHITE);
+        eButton.setForeground(Color.WHITE);
+        fButton.setForeground(Color.WHITE);
+        clrButton.setForeground(Color.WHITE);
+        equButton.setForeground(Color.WHITE);
+
 
         for (int i = 0; i < numButtons.length; i++) {
             numButtons[i] = new JButton(String.valueOf(i));
@@ -75,7 +102,7 @@ public class Calculator implements ActionListener{
 
         panel = new JPanel();
         panel.setBounds(5,100,370,420);
-        panel.setLayout(new GridLayout(4,4,-1,-1));
+        panel.setLayout(new GridLayout(4,5,-1,-1));
         panel.setBackground(Color.DARK_GRAY);
         panel.setBorder(null);
         panel.setForeground(Color.WHITE);
@@ -83,22 +110,27 @@ public class Calculator implements ActionListener{
 
 
         // adding elements to panel
+        panel.add(aButton);
         panel.add(numButtons[1]);
 		panel.add(numButtons[2]);
 		panel.add(numButtons[3]);
-		panel.add(addButton);
+        panel.add(clrButton);
+        panel.add(bButton);
 		panel.add(numButtons[4]);
 		panel.add(numButtons[5]);
 		panel.add(numButtons[6]);
-		panel.add(subButton);
+		panel.add(mulButton);
+        panel.add(cButton);
 		panel.add(numButtons[7]);
 		panel.add(numButtons[8]);
 		panel.add(numButtons[9]);
-		panel.add(mulButton);
+		panel.add(subButton);
+        panel.add(dButton);
 		panel.add(decButton);
 		panel.add(numButtons[0]);
 		panel.add(equButton);
 		panel.add(divButton);
+
 
 
         //adding elements to frame
