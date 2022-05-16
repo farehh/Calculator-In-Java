d864b7174f9ee3f44ca50f0ede654adb289260ed
Fareh Iqbal <fareh.legend@gmail.com>
2022-05-16 18:14:44 +0500

    updates

--- button.java
+++ button.java
@@ -2,10 +2,10 @@ import java.awt.Color;
 import java.awt.*;
 import javax.swing.JButton;
 
-public class button extends JButton{
+public class Button extends JButton{
     Font font= new Font("Courier", Font.BOLD, 20);
 
-    button(String text){
+    Button(String text){
         setText(text);
         setBackground(Color.DARK_GRAY);
         setForeground(Color.WHITE);
